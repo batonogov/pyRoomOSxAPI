8a6a1173e244c02c9868b4b748e9d03aba7d5224
Fedor Batonogov <batonogov@icloud.com>
2022-08-26 20:31:42 +0300

    Save data to file

--- pyRoomOSxAPI/xCommand.py
+++ pyRoomOSxAPI/xCommand.py
@@ -1,201 +1,49 @@
+class Top_Level:
+    def __init__(self) -> str:
+        pass
+
 class Audio:
     def __init__(self) -> str:
         pass
 
-    def Diagnostics(MeasurementLength=6, Output='HDMI', Volume=20):
-        '''
-            This command measures the audio delay/latency in a device that is connected to the video conferencing device. A typical use case it to measure the delay in a TV connected to the video conferencing device via the HDMI connector. If the delay in a TV is too high, the real-time experience of a video call will be substantially degraded. If the delay is more than 50 ms we recommend the user to find a TV setting that has shorter delay. Typical TV settings that can reduce the delay are: Gaming Mode and...
-            
-            ### MeasurementLength:
-            Integer 1 - 100 Default: 6
-            The length of the measurement in number of seconds. In a noisy environment a longer measurement duration will give a more reliable and robust result.
-            
-            ### Output:
-            Literal HDMI, Line, Internal, Headset, All Default: HDMI
-            All: Measure the maximum delay of all outputs. HDMI: Measure the delay of the device connected to the HDMI output. Headset: Measure the delay of the headset (only available on video conferencing devices that have a headset). Internal: Measure the delay of the internal loudspeaker (only available on video conferencing devices that have an internal loudspeaker). Line: Measure the delay of the device connected to the Line output (only available on video conferencing devices that have a Line output).
-            
-            ### Volume:
-            Integer 0 - 50 Default: 20
-            The volume of the test signal. In a noisy environment a higher volume gives more reliable and robust results.
-
-            ---
-            
-            ```
-            Back-end: Any
-            User roles: Admin, User
-            Products: Room Series
-            Privacy impacting: No
-            ```
-        '''
-
-        return f'xCommand Audio Diagnostics MeasureDelay MeasurementLength: {MeasurementLength}, Output: {Output}, Volume: {Volume}'
-
-    def Diagnostics_Advanced(Channel=0, MeasurementLength=3, Output='All', Volume=20):
-        '''
-            This command sends out a noise signal sequentially on all audio output connectors and measures the room impulse response (RIR) between the output and the microphones. If a RIR is detected, the detected number of microphones (input), the detected number of output connectors, and the detected delay between the output and input is reported back. Example: xCommand Audio Diagnostics Advanced Run Volume: 50 MeasurementLength: 1 Result returned -> OK *r AdvancedRunResult (status=OK): *r AdvancedRunResu...
-
-            ### Channel
-            Integer 0 - 13 Default: 0
-            The channel number, from left to right, of the selected output.
-
-            ### MeasurementLength
-            Integer 1 - 100 Default: 3
-            The length of the measurement in number of seconds. In a noisy environment a longer measurement duration will give a more reliable and robust result.
-            
-            ### Output
-            Literal HDMI, Line, Internal, Headset, All Default: All
-            The output where the test signal should be directed. All: Measure the RIR on all outputs. HDMI: Measure the room impulse response (RIR) of the device connected to the HDMI output. Headset: Measure the RIR of the headset (only available on video conferencing devices that have a headset). Internal: Measure the RIR of the internal loudspeaker (only available on video conferencing devices that have an internal loudspeaker). Line: Measure the RIR of the device connected to the Line output (only available on video conferencing devices that have a Line output).
-            
-            ### Volume
-            Integer 0 - 50 Default: 20
-            The volume of the test signal. In a noisy environment a higher volume gives more reliable and robust results.
-            
-            ---
-
-            ```
-            Back-end: Any
-            User roles: Admin, User
-            Products: Room Series
-            Privacy impacting: No
-            ```
-        '''
-
-        return f'xCommand Audio Diagnostics Advanced Run Channel: {Channel}, MeasurementLength: {MeasurementLength}, Output: {Output}, Volume: {Volume}'
-
-    class Diagnostics_AecReverb():
-        def Reset():
-            '''
-                Reset the acoustic echo cancellation. This command is useful when making changes in the acoustical treatment of the room. All previous adaptations are cleared and a new measurement of the reverberation time is made. This is not allowed during a call.
-            
-                ---
-
-                ```
-                Back-end: Any
-                User roles: Admin, User
-                Products: Board Series, Desk, Desk Hub, Desk Limited, Desk Mini, Desk Pro, Room Series
-                Privacy impacting: No
-                ```
-            '''
-            
-            return f'xCommand Audio Diagnostics AecReverb Reset'
-
-        def Run():
-            '''
-                The command uses the acoustic echo canceller to give an estimate of the reverberation time in the room. This is done transparently, without interruption of the normal operation of the endpoint.
-            
-                ---
-
-                ```
-                Back-end: Any
-                User roles: Admin, User
-                Products: Board Series, Desk, Desk Hub, Desk Limited, Desk Mini, Desk Pro, Room Series
-                Privacy impacting: No
-                ```
-            '''
-
-            return f'xCommand Audio Diagnostics AecReverb Run'
-
-    class Microphones():
-        def Mute():
-            '''
-                Mute all microphones.
-
-                ---
-
-                ```
-                Back-end: Any
-                User roles: Admin, Integrator, User
-                Products: Board Series, Desk, Desk Hub, Desk Limited, Desk Mini, Desk Pro, Room Series
-                Privacy impacting: ⚠️ Yes
-                ```
-            '''
-
-            return f'xCommand Audio Microphones Mute'
-
-        def ToggleMute():
-            '''
-                Toggle the microphone between muted and unmuted.
-
-                ---
-
-                ```
-                No parameters
-                Back-end: Any
-                User roles: Admin, Integrator, User
-                Products: Board Series, Desk, Desk Hub, Desk Limited, Desk Mini, Desk Pro, Room Series
-                Privacy impacting: ⚠️ Yes
-                ```
-            '''
-            
-            return f'xCommand Audio Microphones ToggleMute'
-
-        def Unmute():
-            '''
-                Unmute all microphones.
-
-                ---
-
-                ```
-                No parameters
-                Back-end	Any
-                User roles	Admin, Integrator, User
-                Products	Board Series, Desk, Desk Hub, Desk Limited, Desk Mini, Desk Pro, Room Series
-                Privacy impacting	⚠️ Yes
-                ```
-            '''
-            
-            return f'xCommand Audio Microphones Unmute'
-
-
-    def Microphones_MusicMode(action=None):
-        '''
-            ## Actions:
-
-            ### Start
-
-            Start using MusicMode in the current call. Music mode allows the dynamic range of music go through. When Music mode is in use, sound level variations are transmitted intact and the noise filtering is kept to a minimum. MusicMode is automatically turned off when the call ends.
-
-            ### Stop
-
-            Stop using MusicMode in the current call.
-        '''
-
-        if action:
-            return f'{__name__.split(".")[-1]} {Audio.__name__} Microphones MusicMode {action}'
-
-    def Microphones_NoiseRemoval(action=None):
-        '''
-        ## Actions:
-
-        ### Activate
-
-        Activate noise removal on the device. For this to take effect, you need to enable xConfiguration Audio Microphones NoiseRemoval Mode to enable the noise removal feature on the device.
-
-        ### Deactivate
-
-        Deactivate noise removal on the device.
-        '''
-
-        if action:
-            return f'{__name__.split(".")[-1]} {Audio.__name__} Microphones NoiseRemoval {action}'
-
-    # def Sound():
-    #     '''
-    #         Play a specified audio sound.
-    #     '''
-    #     pass
-
-    class Sound():
-        def Play():
-            return 1
-
-    def SoundsAndAlerts_Ringtone():
+    class Microphones:
         pass
 
-    def Volume(action='None'):
-        return f'{__name__.split(".")[-1]} {Audio.__name__} {Audio.Volume.__name__} {action}'
+        def Mute():
+            pass
 
-    def VuMeter():
+        def ToggleMute():
+            pass
+
+        def Unmute():
+            pass
+
+    class Sound:
+        pass
+
+        def Stop():
+            pass
+
+    class Volume:
+        pass
+
+        def Mute():
+            pass
+
+        def ToggleMute():
+            pass
+
+        def Unmute():
+            pass
+
+    class VuMeter:
+        pass
+
+        def StopAll():
+            pass
+
+class Bluetooth:
+    def __init__(self) -> str:
         pass
 
 class Bookings:
@@ -206,45 +54,337 @@ class Call:
     def __init__(self) -> str:
         pass
 
+class CallHistory:
+    def __init__(self) -> str:
+        pass
+
+class CallLog:
+    def __init__(self) -> str:
+        pass
+
+class CallTransfer:
+    def __init__(self) -> str:
+        pass
+
+class Camera:
+    def __init__(self) -> str:
+        pass
+
+class Cameras:
+    def __init__(self) -> str:
+        pass
+
+    class SpeakerTrack:
+        pass
+
+        def Activate():
+            pass
+
+        def Deactivate():
+            pass
+
+class Capabilities:
+    def __init__(self) -> str:
+        pass
+
+class Conference:
+    def __init__(self) -> str:
+        pass
+
+    class SpeakerLock:
+        pass
+
+        def Release():
+            pass
+
+class Diagnostics:
+    def __init__(self) -> str:
+        pass
+
+class FacilityService:
+    def __init__(self) -> str:
+        pass
+
+class FarEndMessage:
+    def __init__(self) -> str:
+        pass
+
+class FeccReceive:
+    def __init__(self) -> str:
+        pass
+
+class Files:
+    def __init__(self) -> str:
+        pass
+
+class H320:
+    def __init__(self) -> str:
+        pass
+
+class H323:
+    def __init__(self) -> str:
+        pass
+
+class HttpClient:
+    def __init__(self) -> str:
+        pass
+
+class HttpFeedback:
+    def __init__(self) -> str:
+        pass
+
+class ICE:
+    def __init__(self) -> str:
+        pass
+
+class Logging:
+    def __init__(self) -> str:
+        pass
+
+class Macros:
+    def __init__(self) -> str:
+        pass
+
+    class Log:
+        pass
+
+        def Clear():
+            pass
+
+    class Macro:
+        pass
+
+        def RemoveAll():
+            pass
+
+    class Runtime:
+        pass
+
+        def Restart():
+            pass
+
+        def Start():
+            pass
+
+        def Status():
+            pass
+
+        def Stop():
+            pass
+
+class MediaChannels:
+    def __init__(self) -> str:
+        pass
+
+class Message:
+    def __init__(self) -> str:
+        pass
+
+class Network:
+    def __init__(self) -> str:
+        pass
+
+class NetworkServices:
+    def __init__(self) -> str:
+        pass
+
+class Peripherals:
+    def __init__(self) -> str:
+        pass
+
+class Phonebook:
+    def __init__(self) -> str:
+        pass
+
+class Presentation:
+    def __init__(self) -> str:
+        pass
+
+class Provisioning:
+    def __init__(self) -> str:
+        pass
+
+class Proximity:
+    def __init__(self) -> str:
+        pass
+
+    class Services:
+        pass
+
+        def Activate():
+            pass
+
+        def Deactivate():
+            pass
+
+class RoomAnalytics:
+    def __init__(self) -> str:
+        pass
+
+class RoomCleanup:
+    def __init__(self) -> str:
+        pass
+
+class RoomPreset:
+    def __init__(self) -> str:
+        pass
+
+class RoomReset:
+    def __init__(self) -> str:
+        pass
+
+class RoomScheduler:
+    def __init__(self) -> str:
+        pass
+
+class SIP:
+    def __init__(self) -> str:
+        pass
+
+class Security:
+    def __init__(self) -> str:
+        pass
+
+    class Ciphers:
+        pass
+
+        def List():
+            pass
+
+    class Session:
+        pass
+
+        def Get():
+            pass
+
+        def List():
+            pass
+
+class SerialPort:
+    def __init__(self) -> str:
+        pass
+
+class Standby:
+    def __init__(self) -> str:
+        pass
+
+class SystemUnit:
+    def __init__(self) -> str:
+        pass
+
+    class DeveloperPreview:
+        pass
+
+        def Activate():
+            pass
+
+        def Deactivate():
+            pass
+
+    class FirstTimeWizard:
+        pass
+
+        def Stop():
+            pass
+
+    class Notifications:
+        pass
+
+        def RemoveAll():
+            pass
+
+    class OptionKey:
+        pass
+
+        def List():
+            pass
+
+    class SignInBanner:
+        pass
+
+        def Clear():
+            pass
+
+        def Get():
+            pass
+
+    class WelcomeBanner:
+        pass
+
+        def Clear():
+            pass
+
+        def Get():
+            pass
+
+class Time:
+    def __init__(self) -> str:
+        pass
+
+    class DateTime:
+        pass
+
+        def Get():
+            pass
+
+class UserInterface:
+    def __init__(self) -> str:
+        pass
+
+    class Branding:
+        pass
+
+        def Clear():
+            pass
+
+    class WallpaperBundle:
+        pass
+
+        def Clear():
+            pass
+
+        def List():
+            pass
+
+class UserManagement:
+    def __init__(self) -> str:
+        pass
+
+    class RemoteSupportUser:
+        pass
+
+        def Delete():
+            pass
+
+        def GetState():
+            pass
+
 class Video:
     def __init__(self) -> str:
         pass
 
-    def ActiveSpeakerPIP():
+class VoiceControl:
+    def __init__(self) -> str:
         pass
 
-    def CEC_Input():
+class WebEngine:
+    def __init__(self) -> str:
         pass
 
-    def CEC_Output():
+class WebRTC:
+    def __init__(self) -> str:
         pass
 
-    def Graphics():
+class Webex:
+    def __init__(self) -> str:
         pass
 
-    def Graphics_Text():
+    class Registration:
         pass
 
-    def Input():
-        pass
+        def Cancel():
+            pass
 
-    def Input_MainVideo():
-        pass
+        def Logout():
+            pass
 
-    def Layout():
-        pass
-
-    def Layout_LayoutFamily():
-        pass
-
-    def Matrix():
-        pass
-
-    def PresentationPIP():
-        pass
-
-    def PresentationView():
-        pass
-
-    def Selfview():
-        pass
--- web.py
+++ web.py
@@ -1,70 +1,57 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time, datetime
+import time
 
 
 # Запрос перечня ссылок на группы комманд
-def xapi_cmd(url='https://roomos.cisco.com/xapi?Product=hopen&Type=Command', wait=5):
-    driver = webdriver.Chrome()
-    driver.get(url)
-    time.sleep(1)
-    return [c for c in driver.find_element(By.CLASS_NAME, 'sub-menu').text.split() if c.isalnum()]
+def xapi_cmd(url='https://roomos.cisco.com/xapi?Product=hopen&Type=Command', output_file_name=None):
+    with open(f'pyRoomOSxAPI/{output_file_name}', 'w') as f:
+        driver = webdriver.Chrome()
+        driver.get(url)
+        time.sleep(1)
+        reference = [c for c in driver.find_element(By.CLASS_NAME, 'sub-menu').text.split('\n')]
+        for i in range(len(reference)):
+            driver.find_element(By.XPATH, f'//a[contains(@href,"/xapi/domain/?domain={reference[i]}")]').click()
+            command_line = ['.'.join(p) for p in [c.split()[1:] for c in [c.text for c in driver.find_elements(By.CLASS_NAME, 'node-path')]]]
+            f.write(f'class {reference[i].replace(" ", "_").replace("(", "").replace(")", "")}:\n')
+            f.write(f'    def __init__(self) -> str:\n        pass\n\n')
+            temp = []
+            for i in range(len(command_line)):
+                driver.find_element(By.XPATH, f'//a[contains(@href,"/xapi/Command.{command_line[i]}/")]').click()
 
-# Генерация ссылки на список существущих комманд в группе
-def link_generator(group) -> str:
-    return f'https://roomos.cisco.com/xapi/domain/?domain={group}&Product=hopen&Type=Command'
+                try:
+                    driver.find_element(By.CLASS_NAME, 'switcher').click()
+                    switcher = driver.find_element(By.CLASS_NAME, 'language-javascript').text.split()
+                    print(len(switcher), f'{switcher}\n')
+                    name = switcher[2]
+                    if len(switcher) == 4:
+                        if name not in temp:
+                            temp.append(name)
+                            f.write(f'    class {name}:\n        pass\n\n')
+                        f.write(f'        def {switcher[3]}():\n            pass\n\n')
 
-# Получение функционала в зависимости от класса
-def get_info(url, name=None, driver_method=None, action=None):
-    driver = webdriver.Chrome()
-    driver.get(url)
-    time.sleep(1)
-    if driver_method == 'find_elements':
-        return [xCommand.text for xCommand in driver.find_elements(By.CLASS_NAME, name)]
-    elif driver_method == 'find_element':
-        if action:
-            commandline_btn = driver.find_element(By.CLASS_NAME, name)
-            commandline_btn.click()
-            return driver.find_element(By.CLASS_NAME, 'language-javascript').text
-        return driver.find_element(By.CLASS_NAME, name).text
+                except Exception as e:
+                    print(e)
 
-# Генерация ссылки на описание комманды
-def link_description_generator(command) -> str:
-    return f'https://roomos.cisco.com/xapi/Command.{command}?p=havella'
-    
-groups = xapi_cmd()
-# print('groups:', groups)
+                # try:
+                #     info = driver.find_element(By.CLASS_NAME, 'info').text
+                #     print(f'{info}\n')
+                #     f.write(f'{info}\n')
+                # except:
+                #     pass
 
-links = [link_generator(g) for g in groups]
-# print('links:', links)
+                # try:
+                #     param = driver.find_element(By.CLASS_NAME, 'param').text
+                #     print(f'{param}\n')
+                #     f.write(f'{param}\n')
+                # except:
+                #     pass
 
-commands = [c.split()[1:] for c in [get_info(url=u, name='node-path', driver_method='find_elements') for u in links][0]]
-# print('commands:', commands)
+                # try:
+                #     props = driver.find_element(By.CLASS_NAME, 'props').text
+                #     print(f'{props}\n')
+                #     f.write(f'{props}\n')
+                # except:
+                #     pass
 
-valhalla = [link_description_generator('.'.join(p)) for p in commands]
-# print('valhalla:', valhalla)
-
-for u in valhalla:
-    with open('test.txt', 'a') as f:
-        try:
-            name = get_info(url=u, name="switcher", driver_method='find_element', action=True)
-            f.write(f'{name}\n\n')
-            print(name)
-        except:
-            pass
-        
-        try:
-            print()
-            info = get_info(url=u, name="info", driver_method='find_element')
-            f.write(f'{info}\n\n')
-            print(info)
-        except:
-            pass
-
-        try:
-            print()
-            require = get_info(url=u, name="param", driver_method='find_element')
-            f.write(f'{require}\n\n')
-            print(require)
-        except:
-            pass
+xapi_cmd(output_file_name='xCommand.py')
