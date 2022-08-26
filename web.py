3cc90bf5836c06995d3f36eb073829992f9ba6da
Fedor Batonogov <batonogov@icloud.com>
2022-08-26 20:47:43 +0300

    Добавлены переборы

--- web.py
+++ web.py
@@ -4,12 +4,13 @@ import time
 
 
 # Запрос перечня ссылок на группы комманд
-def xapi_cmd(url='https://roomos.cisco.com/xapi?Product=hopen&Type=Command', output_file_name=None):
+def xapi_cmd(url='https://roomos.cisco.com/xapi?Product=hopen', output_file_name=None):
     with open(f'pyRoomOSxAPI/{output_file_name}', 'w') as f:
         driver = webdriver.Chrome()
         driver.get(url)
         time.sleep(1)
         reference = [c for c in driver.find_element(By.CLASS_NAME, 'sub-menu').text.split('\n')]
+        driver.find_element(By.CSS_SELECTOR, "#app > div > form > div.type-buttons.filter > button:nth-child(2)").click()
         for i in range(len(reference)):
             driver.find_element(By.XPATH, f'//a[contains(@href,"/xapi/domain/?domain={reference[i]}")]').click()
             command_line = ['.'.join(p) for p in [c.split()[1:] for c in [c.text for c in driver.find_elements(By.CLASS_NAME, 'node-path')]]]
@@ -54,4 +55,27 @@ def xapi_cmd(url='https://roomos.cisco.com/xapi?Product=hopen&Type=Command', out
                 # except:
                 #     pass
 
+    # Перебор xConfiguration   
+    driver.find_element(By.CSS_SELECTOR, "#app > div > form > div.type-buttons.filter > button:nth-child(3)").click()
+    for i in range(len(reference)):
+        driver.find_element(By.XPATH, f'//a[contains(@href,"/xapi/domain/?domain={reference[i]}")]').click()
+        cfg_line = ['.'.join(p) for p in [c.split()[1:] for c in [c.text for c in driver.find_elements(By.CLASS_NAME, 'node-path')]]]
+        for i in range(len(cfg_line)):
+            driver.find_element(By.XPATH, f'//a[contains(@href,"/xapi/Configuration.{cfg_line[i]}/")]').click()
+    # Перебор xStatus
+    driver.find_element(By.CSS_SELECTOR, "#app > div > form > div.type-buttons.filter > button:nth-child(4)").click()
+    for i in range(len(reference)):
+        driver.find_element(By.XPATH, f'//a[contains(@href,"/xapi/domain/?domain={reference[i]}")]').click()
+        status_line = ['.'.join(p) for p in [c.split()[1:] for c in [c.text for c in driver.find_elements(By.CLASS_NAME, 'node-path')]]]
+        for i in range(len(status_line)):
+            driver.find_element(By.XPATH, f'//a[contains(@href,"/xapi/Status.{status_line[i]}/")]').click()
+
+    # Перебор Event
+    driver.find_element(By.CSS_SELECTOR, "#app > div > form > div.type-buttons.filter > button:nth-child(5)").click()
+    for i in range(len(reference)):
+        driver.find_element(By.XPATH, f'//a[contains(@href,"/xapi/domain/?domain={reference[i]}")]').click()
+        event_line = ['.'.join(p) for p in [c.split()[1:] for c in [c.text for c in driver.find_elements(By.CLASS_NAME, 'node-path')]]]
+        for i in range(len(event_line)):
+            driver.find_element(By.XPATH, f'//a[contains(@href,"/xapi/Event.{event_line[i]}/")]').click()
+            
 xapi_cmd(output_file_name='xCommand.py')
